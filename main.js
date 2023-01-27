9f5a28b574f92df2c7e48535d0fcc636a05f600f
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-01-27 21:03:06 +0000

    Modified colors and added transitions to elements for change light and dark mode

--- main.js
+++ main.js
@@ -1,32 +1,34 @@
 const $nav = document.querySelector('nav');
-const $sun = document.querySelector('.sun');
+const $sunMoon = document.querySelector('.sun-moon');
 
 let day = true;
 // eslint-disable-next-line
 function modeChange() {
   if (day) {
     toggleNav();
-    document.documentElement.style.setProperty('--main', '#fff');
-    document.documentElement.style.setProperty('--text', '#000');
-    document.documentElement.style.setProperty('--button', '#000');
+    document.documentElement.style.setProperty('--main', '#70c4c6');
+    document.documentElement.style.setProperty('--text', '#1f303a');
+    document.documentElement.style.setProperty('--button', '#304a58');
     document.documentElement.style.setProperty('--b-text', '#fff');
     document.documentElement.style.setProperty('--sky', '#70c4c6');
     document.documentElement.style.setProperty('--mountain', '#7ac1e4');
     document.documentElement.style.setProperty('--mountain-shadow', '#4f7b91');
     document.documentElement.style.setProperty('--snow', '#ddd');
     document.documentElement.style.setProperty('--snow-shadow', '#ccc');
+    document.documentElement.style.setProperty('--light', '#ffa');
     day = false;
   } else {
     toggleNav();
-    document.documentElement.style.setProperty('--main', '#212529');
-    document.documentElement.style.setProperty('--text', '#70c4c6');
-    document.documentElement.style.setProperty('--button', '#fff');
+    document.documentElement.style.setProperty('--main', '#1f303a');
+    document.documentElement.style.setProperty('--text', '#fff');
+    document.documentElement.style.setProperty('--button', '#70c4c6');
     document.documentElement.style.setProperty('--b-text', '#000');
     document.documentElement.style.setProperty('--sky', '#354a4a');
     document.documentElement.style.setProperty('--mountain', '#4f7b91');
     document.documentElement.style.setProperty('--mountain-shadow', '#304a58');
     document.documentElement.style.setProperty('--snow', '#ccc');
     document.documentElement.style.setProperty('--snow-shadow', '#bbb');
+    document.documentElement.style.setProperty('--light', '#fff');
     day = true;
   }
 }
@@ -38,4 +40,4 @@ function toggleNav() {
   $nav.classList.toggle('bg-light');
 }
 
-$sun.addEventListener('click', modeChange);
+$sunMoon.addEventListener('click', modeChange);
