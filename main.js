aefedcf775dfa57f18d26aac3a909229df5e653b
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-01-27 18:38:10 +0000

    Defined function to change modes -- tested on navbar

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
-// eslint-disable-next-line
 const $nav = document.querySelector('nav');
+
+// eslint-disable-next-line
+function modeChange() {
+  $nav.classList.toggle('navbar-dark');
+  $nav.classList.toggle('navbar-white');
+  $nav.classList.toggle('bg-dark');
+  $nav.classList.toggle('bg-light');
+}
