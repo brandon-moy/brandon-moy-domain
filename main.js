35ffe40810bc192f79b5edd84ce8159135e49f69
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-02-13 19:55:52 +0000

    Removed part of observer that would hide element again, only want to hide and show effect one time

--- main.js
+++ main.js
@@ -41,8 +41,6 @@ const observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
       entry.target.classList.add('visible');
-    } else {
-      entry.target.classList.remove('visible');
     }
   });
 });
